f01cf2499a5eb5bf0467aa4dfa48e5f3788dcfab
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-05-22 17:42:48 +0000

    yyyyyyyyyy

--- src/views/admin/videoChatRoom/App.jsx
+++ src/views/admin/videoChatRoom/App.jsx
@@ -1,66 +1,11 @@
 import * as React from 'react';
-import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 import {
   Box,
 } from "@chakra-ui/react";
-
-function randomID(len) {
-  let result = '';
-  if (result) return result;
-  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
-    maxPos = chars.length,
-    i;
-  len = len || 5;
-  for (i = 0; i < len; i++) {
-    result += chars.charAt(Math.floor(Math.random() * maxPos));
-  }
-  return result;
-}
-
-export function getUrlParams(
-  url = window.location.href
-) {
-  let urlStr = url.split('?')[1];
-  return new URLSearchParams(urlStr);
-}
-
 export default function App() {
-  const roomID = getUrlParams().get('roomID') || randomID(5);
-  let myMeeting = async (element) => {
-
- // generate Kit Token
- const appID = 696234099;
- const serverSecret = "ccc867713caf911d2d58684e248c9382";
- const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID,  randomID(5),  randomID(5));
-
- // Create instance object from Kit Token.
- const zp = ZegoUIKitPrebuilt.create(kitToken);
- // start the call
- zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Personal link',
-            url:
-             window.location.protocol + '//' + 
-             window.location.host + window.location.pathname +
-              '?roomID=' +
-              roomID,
-          },
-        ],
-        scenario: {
-         mode: ZegoUIKitPrebuilt.VideoConference,
-        },
-   });
-  };
-
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-    <div
-      className="myCallContainer"
-      ref={myMeeting}
-      style={{ width: '100vw', height: '100vh' }}
-    ></div>
+
     </Box>
   );
 }
