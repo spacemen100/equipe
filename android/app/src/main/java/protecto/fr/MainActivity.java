65aeba357020c6b9e28ce56a9959fc703d75aecc
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-07-02 19:40:40 +0000

    bvcbfgcdfffdfv

---
+++ android/app/src/main/java/protecto/fr/MainActivity.java
@@ -0,0 +1,5 @@
+package protecto.fr;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.myapp',
-  appName: 'Protecto',
+  appId: 'protecto.fr',
+  appName: 'protecto',
   webDir: 'build'
 };
 
