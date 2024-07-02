fda410ce66046dc83660d618b72c0e058d0b7541
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-07-02 20:02:10 +0000

    dfbgdfgfdgdfgsdfds

--- src/GPSPositionContext.js
+++ src/GPSPositionContext.js
@@ -11,7 +11,7 @@ export const GPSPositionProvider = ({ children }) => {
   useEffect(() => {
     // Use the geolocation API to get the GPS position
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(
+      const watchId = navigator.geolocation.watchPosition(
         (newPosition) => {
           const { latitude, longitude } = newPosition.coords;
           setGPSPosition({ latitude, longitude });
@@ -19,8 +19,16 @@ export const GPSPositionProvider = ({ children }) => {
         (error) => {
           // Handle errors if needed
           console.error('Error getting GPS position:', error);
+        },
+        {
+          enableHighAccuracy: true, // Use high accuracy if available
+          timeout: 5000, // Timeout after 5 seconds
+          maximumAge: 0 // Don't use cached position
         }
       );
+
+      // Cleanup function to clear the watchPosition when the component unmounts
+      return () => navigator.geolocation.clearWatch(watchId);
     }
   }, []);
 
