c7eeb1245f2e2ecf9558c8611fdb98dc43290e79
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-08-08 17:53:21 +0000

    cfffffffff

--- src/components/navbar/NavbarLinksAdmin.js
+++ src/components/navbar/NavbarLinksAdmin.js
@@ -6,9 +6,10 @@ import {
   Image,
   Menu,
   MenuList,
-  useColorModeValue,
   IconButton,
-  Badge
+  Badge,
+  Tooltip,
+  useColorMode
 } from '@chakra-ui/react';
 import DropdownMenu from 'components/navbar/searchBar/DropdownMenu';
 import { SidebarResponsive } from 'components/sidebar/Sidebar';
@@ -25,17 +26,21 @@ export default function HeaderLinks(props) {
   const history = useHistory();
 
   // Chakra Color Mode
-  let menuBg = useColorModeValue('white', 'navy.800');
-  const shadow = useColorModeValue(
-    '14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
-    '14px 17px 40px 4px rgba(112, 144, 176, 0.06)'
-  );
+  const { colorMode } = useColorMode();
+  let menuBg = colorMode === 'light' ? 'white' : 'white';
+  const shadow = colorMode === 'light'
+    ? '14px 17px 40px 4px rgba(112, 144, 176, 0.18)'
+    : '14px 17px 40px 4px rgba(112, 144, 176, 0.06)';
+  const bellIconColor = colorMode === 'light' ? 'gray.500' : 'gray.300';
 
   // State for notifications
   const [hasNotification, setHasNotification] = useState(true);
   
   // Get the unread message count from the context
   const unreadCount = useUnreadMessages();
+
+  // Determine the color for the bell icon and badge
+  const notificationColor = "red"; // Set this to the desired color scheme
 
   return (
     <Flex
@@ -78,30 +83,36 @@ export default function HeaderLinks(props) {
         ml="10px"
       />
       <Flex alignItems="center" position="relative">
-        <IconButton
-          aria-label="Notifications"
-          icon={hasNotification ? <VscBellDot /> : <VscBell />}
-          size="lg"
-          variant="ghost"
-          ml="10px"
-          onClick={() => setHasNotification(!hasNotification)} // Toggle notification state for demonstration
-        />
-        <Badge
-          colorScheme="red"
-          borderRadius="full"
-          position="absolute"
-          top="-2px"
-          left="-2px" // Adjust the positioning as needed
-          fontSize="0.7em"
-          minWidth="18px"
-          h="18px"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          zIndex="9999" // Ensure the badge is above other elements
-        >
-          {unreadCount}
-        </Badge>
+        {unreadCount > 0 && (
+          <>
+            <Tooltip label="Merci de lire vos messages dans le chat" hasArrow>
+              <IconButton
+                aria-label="Notifications"
+                icon={hasNotification ? <VscBellDot color={notificationColor} /> : <VscBell color={bellIconColor} />}
+                size="lg"
+                variant="ghost"
+                ml="10px"
+                onClick={() => setHasNotification(!hasNotification)} // Toggle notification state for demonstration
+              />
+            </Tooltip>
+            <Badge
+              colorScheme={notificationColor}
+              borderRadius="full"
+              position="absolute"
+              top="-2px"
+              left="-2px" // Adjust the positioning as needed
+              fontSize="0.7em"
+              minWidth="18px"
+              h="18px"
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              zIndex="9999" // Ensure the badge is above other elements
+            >
+              {unreadCount}
+            </Badge>
+          </>
+        )}
         {hasNotification && (
           <MdSos 
             style={{ color: 'red', marginLeft: '10px', fontSize: '24px', cursor: 'pointer' }} 
