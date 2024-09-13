d40206978d39209741c1c04a650f435ba5f506f8
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-09-13 02:55:59 +0000

    zzzzzzzzzzzzzzzzze

--- src/views/admin/Menu/index.js
+++ src/views/admin/Menu/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Box, SimpleGrid, Text, VStack, IconButton } from "@chakra-ui/react";
-import { FcSelfie, FcCalendar, FcDocument, FcSupport, FcSettings, FcComments, FcEditImage, FcHighPriority } from "react-icons/fc";
+import { FcMenu, FcCalendar, FcDocument, FcSettings, FcComments, FcHighPriority } from "react-icons/fc";
+import { BsGeoAltFill } from "react-icons/bs";
 import { FaMapMarked } from "react-icons/fa"; // Import the map icon
 import { useHistory } from "react-router-dom";
 
@@ -8,19 +9,15 @@ const Menu = () => {
   const history = useHistory();
 
   const menuItems = [
-    { icon: FcSelfie, label: "Gestion opé", path: "/admin/gestion-operationnelle" },
+    { icon: FcMenu, label: "Menu", path: "/admin/default" },
+    { icon: BsGeoAltFill, label: "Géolocalisation", path: "/admin/gestion-operationnelle" },
     { icon: FcCalendar, label: "Emploi du temps", path: "/admin/emploi-du-temps" },
-    { icon: FcDocument, label: "Documents", path: "/admin/documents" },
-    // { icon: FcConferenceCall, label: "Communication", path: "/admin/communication" }, // Communication item hidden
-    { icon: FcSupport, label: "Matériel", path: "/admin/materiels" },
-    { icon: FcSettings, label: "Params", path: "/admin/parametres" },
+    { icon: FcDocument, label: "Documents", path: "/admin/documents-tabs" },
     { icon: FcComments, label: "Chat", path: "/admin/chat" },
-    { icon: FcDocument, label: "Rapport incident", path: "/admin/rapport-incident" },
-    { icon: FcDocument, label: "Fiche Bilan SUAP", path: "/admin/fiche-bilan-suap" },
-    { icon: FcEditImage, label: "Note de frais", path: "/admin/note-de-frais" },
+    { icon: FcSettings, label: "Paramètres", path: "/admin/parametres" },
     { icon: FcHighPriority, label: "SOS Alerte Danger", path: "/admin/sos-alerte-danger" },
-    { icon: FaMapMarked, label: "Carte", path: "/admin/carte" }, // Added link to Carte
-  ];
+    { icon: FaMapMarked, label: "Carte", path: "/admin/carte" }, // Only "Carte" remains, not "Carte zoomée"
+  ];  
 
   return (
     <Box pt={{ base: '180px', md: '80px', xl: '80px' }} textAlign="center">
