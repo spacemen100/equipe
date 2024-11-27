2ca201b0e9658f3f1b9d9861c66daffe957de5d6
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2024-11-27 04:17:56 +0000

    sssssssssssssssd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'protectosystem.fr',
   appName: 'protecto',
-  webDir: 'build'
+  webDir: 'build',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 0
+    }
+    // Ajoutez d'autres configurations de plugins si nécessaire
+  }
 };
 
 export default config;
