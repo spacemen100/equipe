dbfc5a6f88b68b40e3f6eb8a3969045a4e9efb3a
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2025-03-30 23:07:48 +0200

    Update index.jsx

--- src/views/admin/NoteDeFrais/index.jsx
+++ src/views/admin/NoteDeFrais/index.jsx
@@ -1,16 +1,15 @@
-// views/admin/NoteDeFraisComponent.jsx
 import React from 'react';
-import { Box} from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
-import ExpenseReportContainer from './components/etapes/ExpenseReportContainer';
+import DocumentTabs from '../Factures/components/DocumentTabs';
 
 const NoteDeFraisComponent = () => {
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
+      <DocumentTabs/>
       <ExpenseForm/>
       <ExpenseList/>
-      <ExpenseReportContainer/>
     </Box>
   );
 };
